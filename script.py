70ac11c0e03e4a21cfdb8fb47af00453af642b37
Alon Joseph Even <alon1421@gmail.com>
2022-09-10 12:58:44 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 #!/usr/bin/ python
 path = '/home/alon/done/read.txt'
-file = open(path,'r')
+try:
+  file = open(path,'r')
 except IOError:
-file = open(path,'w+')  
+  file = open(path,'w+')  
 
 file.write("Worked")
 file.close()
